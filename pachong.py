fb1091a8f75023d1dbf4d5bd65eff0272d85913a
ShawnQiang <62214878+ShawnQiang1@users.noreply.github.com>
2021-01-22 23:10:09 +0800

    Update pachong.py

--- pachong.py
+++ pachong.py
@@ -3,22 +3,26 @@ import urllib.request as ur
 from bs4 import BeautifulSoup
 import requests
 import time
-addr="http://neunews.neu.edu.cn/tzgg/list.htm"
-key="http://sc.ftqq.com/[SCKEY].send?"
-res = ur.urlopen(addr)
-soup = BeautifulSoup(res, "html.parser")
-aAll = soup.select("div[frag='面板89'] .news_list a")
-first=aAll[0].string
 while True:
-    res=ur.urlopen(addr)
-    soup=BeautifulSoup(res,"html.parser")
-    aAll=soup.select("div[frag='面板89'] .news_list a")
-    if aAll[0].string!=first:
-        first=aAll[0].string
-        link="http://neunews.neu.edu.cn/"+aAll[0]["href"]
-        text=key+"text="+first+"&desp="+link
-        post=requests.post(text)
-        print("已发送消息",aAll[0].string,"!")
-    else:
-        print("无变化！")
-    time.sleep(5)
+    addr="http://neunews.neu.edu.cn/tzgg/list.htm"
+    key="http://sc.ftqq.com/[SCKEY].send?"   #替换你的SCKRY
+    res = ur.urlopen(addr)
+    soup = BeautifulSoup(res, "html.parser")
+    aAll = soup.select("div[frag='面板89'] .news_list a")
+    first=aAll[0].text
+    while True:
+        try:
+            res=ur.urlopen(addr)
+            soup=BeautifulSoup(res,"html.parser")
+            aAll=soup.select("div[frag='面板89'] .news_list a")
+            if aAll[0].text!=first:
+                link="http://neunews.neu.edu.cn/"+aAll[0]["href"]
+                text=key+"text="+first+"&desp="+link
+                post=requests.post(text)
+                print("已发送消息",aAll[0].string,"!")
+                first = aAll[0].text
+            else:
+                print("无变化！")
+            time.sleep(500)
+        except:
+            print("出错了，重来！")
